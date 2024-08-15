b9715557c2434529f62bddcbc45aa11b1cde0458
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-08-15 12:30:04 +0300

    first setup for the project + mainpage

---
+++ Clinet/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import MainPage from "./componentes/MainPage";
+
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  console.log(1111);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ Clinet/src/componentes/MainPage.jsx
@@ -0,0 +1,298 @@
+import React from "react";
+import { useState } from "react";
+
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+//exmaples of produvts
+const products = [
+  // More products...
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 1,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+];
+const incentives = [
+  {
+    name: "Free Shipping",
+    description:
+      "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce/icons/icon-delivery-light.svg",
+  },
+  {
+    name: "24/7 Customer Support",
+    description:
+      "Our AI chat widget is powered by a naive series of if/else statements. Guaranteed to irritate.",
+    imageSrc: "https://tailwindui.com/img/ecommerce/icons/icon-chat-light.svg",
+  },
+  {
+    name: "Fast Shopping Cart",
+    description:
+      "Look how fast that cart is going. What does this mean for the actual experience? I don't know.",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg",
+  },
+];
+function mainPage() {
+  return (
+    <div>
+      <NavBar />
+      <div className="main-header-container">
+        <div className="relative bg-white">
+          <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+            <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-48 xl:col-span-6">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <img
+                  alt="Your Company"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  className="h-11"
+                />
+                <div className="hidden sm:mt-32 sm:flex lg:mt-16">
+                  <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                    Anim aute id magna aliqua ad ad non deserunt sunt.{" "}
+                    <a
+                      href="#"
+                      className="whitespace-nowrap font-semibold text-indigo-600"
+                    >
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
+                </div>
+                <h1 className="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
+                  Data to enrich your online business
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                  occaecat fugiat aliqua.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+              <img
+                alt=""
+                src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
+                className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="top-sellers-container">
+        <div className="bg-white">
+          <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+              Customers also purchased
+            </h2>
+
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+              {products.map((product) => (
+                <div key={product.id} className="group relative">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                    <img
+                      alt={product.imageAlt}
+                      src={product.imageSrc}
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                  <div className="mt-4 flex justify-between">
+                    <div>
+                      <h3 className="text-sm text-gray-700">
+                        <a href={product.href}>
+                          <span
+                            aria-hidden="true"
+                            className="absolute inset-0"
+                          />
+                          {product.name}
+                        </a>
+                      </h3>
+                      <p className="mt-1 text-sm text-gray-500">
+                        {product.color}
+                      </p>
+                    </div>
+                    <p className="text-sm font-medium text-gray-900">
+                      {product.price}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="site-info-contaienr">
+        <div className="bg-gray-50">
+          <div className="mx-auto max-w-7xl py-24 sm:px-2 sm:py-32 lg:px-4">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 px-4 lg:max-w-none lg:grid-cols-3">
+              {incentives.map((incentive) => (
+                <div
+                  key={incentive.name}
+                  className="text-center sm:flex sm:text-left lg:block lg:text-center"
+                >
+                  <div className="sm:flex-shrink-0">
+                    <div className="flow-root">
+                      <img
+                        alt=""
+                        src={incentive.imageSrc}
+                        className="mx-auto h-24 w-28"
+                      />
+                    </div>
+                  </div>
+                  <div className="mt-3 sm:ml-3 sm:mt-0 lg:ml-0 lg:mt-3">
+                    <h3 className="text-sm font-medium text-gray-900">
+                      {incentive.name}
+                    </h3>
+                    <p className="mt-2 text-sm text-gray-500">
+                      {incentive.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="promo-section-continaer">
+        {" "}
+        <div className="bg-white">
+          <div className="overflow-hidden pt-32 sm:pt-14">
+            <div className="bg-gray-800">
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="relative pb-16 pt-48 sm:pb-24">
+                  <div>
+                    <h2
+                      id="sale-heading"
+                      className="text-4xl font-bold tracking-tight text-white md:text-5xl"
+                    >
+                      Final Stock.
+                      <br />
+                      Up to 50% off.
+                    </h2>
+                    <div className="mt-6 text-base">
+                      <a href="#" className="font-semibold text-white">
+                        Shop the sale
+                        <span aria-hidden="true"> &rarr;</span>
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="absolute -top-32 left-1/2 -translate-x-1/2 transform sm:top-6 sm:translate-x-0">
+                    <div className="ml-24 flex min-w-max space-x-6 sm:ml-3 lg:space-x-8">
+                      <div className="flex space-x-6 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
+                        <div className="flex-shrink-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+
+                        <div className="mt-6 flex-shrink-0 sm:mt-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-02.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex space-x-6 sm:-mt-20 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
+                        <div className="flex-shrink-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-favorite-01.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+
+                        <div className="mt-6 flex-shrink-0 sm:mt-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-favorite-02.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+                      </div>
+                      <div className="flex space-x-6 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
+                        <div className="flex-shrink-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+
+                        <div className="mt-6 flex-shrink-0 sm:mt-0">
+                          <img
+                            alt=""
+                            src="https://tailwindui.com/img/ecommerce-images/home-page-03-category-02.jpg"
+                            className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default mainPage;
