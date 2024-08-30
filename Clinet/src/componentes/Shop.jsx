7a58320b11a636c342c98f001cfbc22537e7bff2
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-08-30 13:11:11 +0300

    store page updates , and fixes

--- Clinet/src/componentes/Shop.jsx
+++ Clinet/src/componentes/Shop.jsx
@@ -65,6 +65,8 @@ function Shop() {
   // FIXME: on the small sizes the page renders and becouse of that the err acoord
   // console.log(open);
 
+  console.log(products);
+
   return (
     <div>
       <div className="body-container">
@@ -234,11 +236,20 @@ function Shop() {
                   <h3 className="mt-4 text-sm text-gray-700">
                     {product.title}
                   </h3>
-                  <p className="mt-1 text-lg font-medium text-gray-900">
-                    {product.variants[0]?.price.amount
-                      ? `${product.variants[0].price.amount} ${product.variants[0].price.currencyCode}`
-                      : "Price not available"}
-                  </p>
+                  <div className="flex place-content-between">
+                    <p className="mt-1 text-lg font-medium text-gray-900">
+                      {product.variants[0]?.price.amount
+                        ? `${product.variants[0].price.amount} ${product.variants[0].price.currencyCode}`
+                        : "Price not available"}
+                    </p>
+                    <p className="mt-1 text-lg font-medium text-gray-900  line-through">
+                      {product.variants[0]?.compareAtPrice
+                        ? `${product.variants[0].compareAtPrice.amount} 
+                      
+                        ${product.variants[0].compareAtPrice.currencyCode}`
+                        : "  "}
+                    </p>
+                  </div>
                 </a>
               ))}
             </div>
@@ -247,44 +258,130 @@ function Shop() {
         {/* section */}
         <div className="relative bg-gray-800 px-6 py-32 sm:px-12 sm:py-40 lg:px-16">
           <div className="absolute inset-0 overflow-hidden">
-            {/* <img
-              alt=""
-              src="https://cdn.vox-cdn.com/thumbor/03pr6mrG8uDchXWtTM77sKkuvPM=/0x0:893x539/1400x1050/filters:focal(447x270:448x271)/cdn.vox-cdn.com/uploads/chorus_asset/file/21902907/xbox_lab_design.jpg"
-              className="h-full w-full object-cover object-center"
-            /> */}
             <img
-              // sizes="(min-width: 768px) 100vw, (max-width: 797px) min(100%, 757px), (max-height: 756px) min(100%, 757px), (min-aspect-ratio: 2957/2266) calc((calc(100vh - 175px)) * 1.30494), calc(100vw - 40px)"
-              // srcset="https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=757&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 757w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=1057&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1057w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=1357&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1357w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=1514&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1514w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=1657&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1657w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=1957&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 1957w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2114&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2114w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2257&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2257w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2557&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2557w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2714&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2714w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2857&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2857w, https://images.unsplash.com/photo-1593305841991-05c297ba4575?q=80&amp;w=2957&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D 2957w"
               src="https://images.unsplash.com/photo-1593305841991-05c297ba4575?fm=jpg&amp;q=60&amp;w=3000&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt="a computer screen with a video game on it"
               className="h-full w-full object-cover object-center"
-
-              // class="I7OuT DVW3V L1BOa"
-              // style="aspect-ratio: 2957 / 2266;"
             ></img>
           </div>
           <div
             aria-hidden="true"
             className="absolute inset-0 bg-gray-900 bg-opacity-50"
           />
-          <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
+          <div
+            className="relative mx-auto flex max-w-3xl flex-col items-center text-center"
+            dir="rtl"
+          >
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Long-term thinking
+              קנו את הבקר המעוצב הראשון שלכם
             </h2>
             <p className="mt-3 text-xl text-white">
-              We're committed to responsible, sustainable, and ethical
-              manufacturing. Our small-scale approach allows us to focus on
-              quality and reduce our impact. We're doing our best to delay the
-              inevitable heat-death of the universe.
+              אצלנו, כל בקר מעוצב בקפידה ומיוצר ברמה הגבוהה ביותר. אנו מחויבים
+              לאיכות, עיצוב אישי ועמידה בתקנים האתיים המחמירים ביותר. כל רכישה
+              תומכת ביצירת חוויות גיימינג בלתי נשכחות.
             </p>
             <a
               href="#"
               className="mt-8 block w-full rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
             >
-              Read our story
+              גלו את הקולקציה שלנו
             </a>
           </div>
         </div>
+
+        {/* IDF SECTION */}
+        <section class="py-24 relative">
+          <div class="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div class="w-full md:px-16 px-10 md:pt-16 pt-10 pb-10 bg-gray-900 rounded-2xl flex-col justify-end items-center lg:gap-28 md:gap-16 gap-10 inline-flex">
+              <div class="flex-col justify-end items-center lg:gap-16 gap-10 flex">
+                <img
+                  src="https://pagedone.io/asset/uploads/1717500460.png"
+                  alt="pagedone logo image"
+                />
+                <div class="flex-col justify-center items-center gap-10 flex">
+                  <div class="flex-col justify-start items-center gap-2.5 flex">
+                    <h1 class="text-center text-emerald-400 md:text-6xl text-5xl font-bold font-manrope leading-normal">
+                      IDF SECTIONS CONTROLLERS
+                    </h1>
+                    <h2 class="text-center text-emerald-400 md:text-6xl text-5xl font-bold font-manrope leading-normal">
+                      Coming Soon
+                    </h2>
+                    <p class="text-center text-gray-500 text-base font-normal leading-relaxed">
+                      Just 20 days remaining until the big reveal of our new
+                      product!
+                    </p>
+                  </div>
+                  <div class="flex items-start justify-center w-full gap-2 count-down-main">
+                    <div class="timer flex flex-col gap-0.5">
+                      <div class="">
+                        <h3 class="countdown-element days text-center text-white text-2xl font-bold font-manrope leading-9"></h3>
+                      </div>
+                      <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
+                        DAYS
+                      </p>
+                    </div>
+                    <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
+                      :
+                    </h3>
+                    <div class="timer flex flex-col gap-0.5">
+                      <div class="">
+                        <h3 class="countdown-element hours text-center text-white text-2xl font-bold font-manrope leading-9"></h3>
+                      </div>
+                      <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
+                        HRS
+                      </p>
+                    </div>
+                    <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
+                      :
+                    </h3>
+                    <div class="timer flex flex-col gap-0.5">
+                      <div class="">
+                        <h3 class="countdown-element minutes text-center text-white text-2xl font-bold font-manrope leading-9"></h3>
+                      </div>
+                      <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
+                        MINS
+                      </p>
+                    </div>
+                    <h3 class="w-3 text-center text-gray-500 text-2xl font-medium font-manrope leading-9">
+                      :
+                    </h3>
+                    <div class="timer flex flex-col gap-0.5">
+                      <div class="">
+                        <h3 class="countdown-element seconds text-center text-white text-2xl font-bold font-manrope leading-9"></h3>
+                      </div>
+                      <p class="text-center text-gray-500 text-xs font-normal leading-normal w-full">
+                        SECS
+                      </p>
+                    </div>
+                  </div>
+                  <div class="w-full flex-col justify-center items-center gap-5 flex">
+                    <h6 class="text-center text-emerald-400 text-base font-semibold leading-relaxed">
+                      Launched Date: July 23, 2024
+                    </h6>
+                    <div class="justify-center items-center gap-2.5 flex sm:flex-row flex-col">
+                      {/* <input type="text" class="w-80 focus:outline-none px-3.5 py-2 shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] text-gray-900 placeholder-gray-400 text-sm font-normal leading-relaxed h-10 bg-white rounded-lg border border-gray-200 justify-start items-center gap-1.5 inline-flex" placeholder="Type your mail..."> */}
+                      <button class="sm:w-fit w-full px-3.5 py-2 bg-emerald-400 hover:bg-emerald-600 transition-all duration-700 ease-in-out rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.05)] justify-center items-center flex">
+                        <span class="px-1.5 text-gray-900 text-sm font-medium leading-6 whitespace-nowrap">
+                          Notify Me
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p class="text-center text-gray-500 text-sm font-normal leading-snug">
+                Get in touch with us:{" "}
+                <a
+                  href=""
+                  class="hover:text-gray-100 transition-all duration-700 ease-in-out"
+                >
+                  {" "}
+                  mail@pagedone.com
+                </a>
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
