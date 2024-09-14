a1605daa5a5914170c73ab62cb29e10ce66d539f
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-14 16:59:01 +0300

    vercel componentss update

--- Clinet/vite.config.js
+++ Clinet/vite.config.js
@@ -1,42 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
-import path from "path";
+// import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    compression({
-      algorithm: "gzip", // or 'brotliCompress' for Brotli
-      threshold: 10240, // Compress files larger than 10kB
-    }),
+    // Remove compression plugin
   ],
-  // server: {
-  //   host: true, // This makes the server accessible on your network
-  //   port: 5173, // Keep the default port (or change it if needed)
-  // },
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Suppress specific warnings
-        if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
-        if (warning.code === "THIS_IS_UNDEFINED") return;
-        // Add more conditions if needed
-        warn(warning);
-      },
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@contexts": path.resolve(__dirname, "src/contexts"),
-    },
-  },
 });
+
+// WITH COMPERSSION
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     compression({
+//       algorithm: "gzip", // or 'brotliCompress' for Brotli
+//       threshold: 10240, // Compress files larger than 10kB
+//     }),
+//   ],
+//   // server: {
+//   //   host: true, // This makes the server accessible on your network
+//   //   port: 5173, // Keep the default port (or change it if needed)
+//   // },
+//   build: {
+//     rollupOptions: {
+//       onwarn(warning, warn) {
+//         // Suppress specific warnings
+//         if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
+//         if (warning.code === "THIS_IS_UNDEFINED") return;
+//         // Add more conditions if needed
+//         warn(warning);
+//       },
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes("node_modules")) {
+//             return "vendor";
+//           }
+//         },
+//       },
+//     },
+//   },
+// });
