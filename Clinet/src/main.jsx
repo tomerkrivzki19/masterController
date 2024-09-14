90c587f81f7edad91a71f039207e6ce9c0ee547c
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-14 14:48:12 +0300

    fixed the issue of vercel requirments

--- Clinet/src/main.jsx
+++ Clinet/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CartProvider } from "./contexts/cartContext.jsx";
+// import { CartProvider } from "./contexts/cartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    {/* <CartProvider> */}
+    <App />
+    {/* </CartProvider> */}
   </StrictMode>
 );
