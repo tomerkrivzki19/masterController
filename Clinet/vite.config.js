2c14f8d3fd70a20a8b96bebb0878af15c7face6b
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-14 12:22:02 +0300

    vercel commit

--- Clinet/vite.config.js
+++ Clinet/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // This makes the server accessible on your network
-    port: 5173, // Keep the default port (or change it if needed)
-  },
+  // server: {
+  //   host: true, // This makes the server accessible on your network
+  //   port: 5173, // Keep the default port (or change it if needed)
+  // },
   build: {
     rollupOptions: {
       output: {
