47ade568c2e92a37e0a6ea29cead08fab6904733
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-14 14:42:31 +0300

    comperssion settings to vite filles

--- Clinet/vite.config.js
+++ Clinet/vite.config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip", // or 'brotliCompress' for Brotli
+      threshold: 10240, // Compress files larger than 10kB
+    }),
+  ],
   // server: {
   //   host: true, // This makes the server accessible on your network
   //   port: 5173, // Keep the default port (or change it if needed)
   // },
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes("node_modules")) {
-  //           return "vendor"; // All node_modules are bundled into a 'vendor' chunk
-  //         }
-  //       },
-  //     },
-  //     onwarn(warning, warn) {
-  //       console.warn(warning.message); // Output warnings to the console
-  //       warn(warning);
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
