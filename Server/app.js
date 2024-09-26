32a50f6a74a49790c8be433b438a5ebcfb857d92
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-26 16:32:36 +0300

    cors test

--- Server/app.js
+++ Server/app.js
@@ -17,9 +17,9 @@ const app = express();
 
 app.use(
   cors({
+    origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    origin: "https://master-controller.vercel.app/",
   })
 );
 app.options("*", cors()); //define all the routes
