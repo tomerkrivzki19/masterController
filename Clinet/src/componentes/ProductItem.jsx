77f8960d81de3c915de3c50111a51b316a8f1d46
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2024-09-29 15:44:56 +0300

    swtich the add to cart contect with lorem +animation

--- Clinet/src/componentes/ProductItem.jsx
+++ Clinet/src/componentes/ProductItem.jsx
@@ -48,18 +48,26 @@ function ProductItem() {
 
         setIsFavorite(productIds.includes(decodedId)); // Check the Set directly
       } catch (error) {
-        location.assign("*");
+        navigate("*");
       }
     }
 
     fetchProduct();
   }, [id, navigate, productIds]);
 
+  console.log(loadingIndex);
+
   const handleAddCart = async (variantId, quantity) => {
     setLoadingIndex(false);
+
     try {
       await addToCart(variantId, quantity);
-      setLoadingIndex(true);
+      // setLoadingIndex(false);
+
+      // Show "נוסף לסל" for 6 seconds
+      setTimeout(() => {
+        setLoadingIndex(true); // Reset loadingIndex to show "הוסף לסל" after 6 seconds
+      }, 15000);
     } catch (error) {
       tostify.createToast(
         "error",
@@ -193,7 +201,24 @@ function ProductItem() {
                     className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
                   >
                     {!loadingIndex ? (
-                      <div className="w-5 h-5 border-4 border-white border-t-transparent border-solid rounded-full animate-spin"></div>
+                      // <div className="w-5 h-5 border-4 border-white border-t-transparent border-solid rounded-full animate-spin"></div>
+                      <div className="flex gap-2 animate-fade-left animate-delay-300">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                          />
+                        </svg>
+                        נוסף לסל
+                      </div>
                     ) : (
                       "הוסף לסל"
                     )}
